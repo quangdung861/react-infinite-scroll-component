ece47fa3c3d72373c4304bd32cc4a72adf383ebe
quangdung861 <quangdung861@gmail.com>
2023-07-19 08:35:37 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  border: "1px solid green",
+  margin: 12,
+  padding: 8,
+};
 
 function App() {
+  const [dataSource, setDataSource] = useState(Array.from({ length: 20 }));
+  const [hasMore, setHasMore] = useState(true);
+  const fetchMoreData = () => {
+    if (dataSource.length < 200) {
+      setTimeout(() => {
+        setDataSource(dataSource.concat(Array.from({ length: 20 })));
+      }, 500);
+    } else {
+      setHasMore(false);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <p>
+        Title: <b>InfiniteScroll Tutorial</b>
+      </p>
+      <div id="parentScrollDiv" style={{ height: 500, overflow: "auto", padding: "0 16px" }}>
+        <InfiniteScroll
+          dataLength={dataSource.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={<p>You are all set!</p>}
+          height={500}
+          scrollableTarget="parentScrollDiv"
         >
-          Learn React
-        </a>
-      </header>
+          {dataSource.map((item, index) => {
+            return (
+              <div key={index} style={style}>
+                This is a div #{index + 1} inside InfiniteScroll
+              </div>
+            );
+          })}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
